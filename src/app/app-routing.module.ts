176e2af5ff52c4419c4ae57614e25a0dba081d76
陳鈞 <jim60105@gmail.com>
2021-04-10 16:30:18 +0800

    重構所有頁面至Layout Component下

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,16 +5,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'page1', component: Page1Component },
-  { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
-  { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) }
-];
+  {
+    path: '', component: LayoutComponent, children: [
+      { path: '', component: DashboardComponent },
+      { path: 'page1', component: Page1Component },
+      { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) },
+      { path: 'utilities', loadChildren: () => import('./utilities/utilities.module').then(m => m.UtilitiesModule) }
+    ]},
+    { path: 'login', component: LoginComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { LoginComponent } from './login/login.component';
     Page2Component,
     DashboardComponent,
     LoginComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
