de1e14e5a46a2c197a82f0b4a0eaa139462c2038
陳鈞 <jim60105@gmail.com>
2021-04-10 16:26:36 +0800

    Create login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'page1', component: Page1Component },
   { path: 'page2', component: Page2Component, canActivate: [AuthGuard] },
   { path: 'dashboard', component: DashboardComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     Page1Component,
     Page2Component,
     DashboardComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
