5c47783771716ab57bf4935001c6798fe3c725c9
陳鈞 <jim60105@gmail.com>
2021-04-10 10:25:59 +0800

    useHash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 
 const routes: Routes = [
-{ path: '', component: DashboardComponent },
-{ path: 'page1', component: Page1Component },
-{ path: 'page2', component: Page2Component },
-{ path: 'dashboard', component: DashboardComponent },
+  { path: '', component: DashboardComponent },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+  { path: 'dashboard', component: DashboardComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
